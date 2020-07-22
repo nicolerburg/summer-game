b324249450b1458b6bcc3b352bfef1c87c86c7b2
Nicole Burg <32913338+nicolerburg@users.noreply.github.com>
2020-07-22 19:14:02 -0400

    Making objects movable

--- Assets/Scripts/Made By Me/Movable_Object.cs
+++ Assets/Scripts/Made By Me/Movable_Object.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Diagnostics;
 using UnityEngine;
 
 public class Movable_Object : MonoBehaviour
@@ -12,44 +13,65 @@ public class Movable_Object : MonoBehaviour
 
     //private Rigidbody2D rigidBody;
     private Collider2D collider;
+    private Collider2D trigger;
+    private Collider2D collidingWith;
     private PlayerMovement_Burg movement;
     private bool isInteracting;
-    private float difference;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        //rigidBody = GetComponent<Rigidbody2D>();
-        collider = GetComponent<Collider2D>();
         movement = FindObjectOfType<PlayerMovement_Burg>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButton(0)) {
-            isInteracting = true;
-        } else {
-            isInteracting = false;
+        Collider2D[] colliders = GetComponents<Collider2D>();
+        for (int i = 0; i < colliders.Length; i++) {
+            Collider2D col = colliders[i];
+            if (col.isTrigger) {
+                trigger = col;
+            } else { collider = col; }
         }
     }
 
-    // On collision with player object will follow player's x position
-    public void OnTriggerStay2D(Collider2D col) {
-        if (col.gameObject.name == "Player" && isInteracting) {
-            //movement.changeSpeed(speedReduction);
-            difference = GameObject.Find("Player").transform.position.x - this.transform.position.x;
-            if (difference < 0) {
-                this.transform.position = new Vector3(GameObject.Find("Player").transform.position.x + 1.1f, this.transform.position.y, this.transform.position.z);
+    // Update is called once per frame
+    void FixedUpdate() {
+        if (collidingWith) {
+            GameObject obj = collidingWith.gameObject;
+            if (obj.name == "Player") {
+                if (Input.GetMouseButton(0)) {
+                    //movement.changeSpeed(speedReduction);
+                    float delta = 0.1f;
+                    Vector3 boxPos = this.transform.position;
+                    Vector2 boxSize = collider.bounds.size;
+                    Vector3 playerPos = obj.transform.position;
+                    Vector2 playerSize = collidingWith.bounds.size;
+                    Vector3 playerVel = obj.GetComponent<Rigidbody2D>().velocity;
+                    UnityEngine.Debug.Log(Mathf.Sign(playerVel.x));
+                    //UnityEngine.Debug.Log((playerPos.x + 0.5f * playerSize.x).ToString() + " vs. " + (boxPos.x - 0.5f * boxSize.x).ToString());
+                    //Debug.DrawRay(collider.bounds.center + new Vector3(collider.bounds.extents.x, 0), Vector2.down * (collider.bounds.extents.y + boxCastDepth), color);
+                    if (playerPos.x + 0.5f * playerSize.x <= boxPos.x - 0.5f * boxSize.x) { //player is on left
+                        //Debug.DrawRay(new Vector3(playerPos.x + 0.5f * playerSize.x, playerPos.y, playerPos.z), new Vector3(boxPos.x - 0.5f * boxSize.x - playerPos.x + 0.5f * playerSize.x, 0, 0), Color.green);
+                        boxPos.x = playerPos.x + 0.5f * playerSize.x + 0.5f * boxSize.x + delta + playerVel.x / 100;
+                    }
+                    else if (playerPos.x - 0.5f * playerSize.x >= boxPos.x + 0.5f * boxSize.x) { //player is on right
+                        //Debug.DrawRay(new Vector3(playerPos.x - 0.5f * playerSize.x, playerPos.y, playerPos.z), new Vector3(boxPos.x + 0.5f * boxSize.x + playerPos.x - 0.5f * playerSize.x, 0, 0), Color.green);
+                        boxPos.x = playerPos.x - 0.5f * playerSize.x - 0.5f * boxSize.x - delta + playerVel.x / 100;
+                        UnityEngine.Debug.Log("Is on right and clicking.");
+                    }
+                    this.transform.position = boxPos;
+                    //rigidBody.velocity = new Vector2(movement.walkSpeed, rigidBody.velocity.y);
+                    //rigidBody.velocity.x = GameObject.Find("Player").GetComponent<PlayerMovement_Burg>().walkSpeed;
+                }
             }
-            else if (difference > 0){
-                this.transform.position = new Vector3(GameObject.Find("Player").transform.position.x - 1, this.transform.position.y, this.transform.position.z); ;
-            }
-            //this.transform.position = GameObject.Find("Player").transform.position;
-            //rigidBody.velocity = new Vector2(movement.walkSpeed, rigidBody.velocity.y);
-            //rigidBody.velocity.x = GameObject.Find("Player").GetComponent<PlayerMovement_Burg>().walkSpeed;
-        } else { movement.changeSpeed(1); }
-
+        }
     }
+
+    public void OnTriggerEnter2D(Collider2D otherCol) {
+        collidingWith = otherCol;
+    }
+
+    public void OnTriggerExit2D(Collider2D otherCol) {
+        collidingWith = null;
+    }
+
+
 }
