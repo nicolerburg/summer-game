34b7300ec31d60d8ace673973d83af85614e4ba5
Nicole Burg <32913338+nicolerburg@users.noreply.github.com>
2020-07-23 15:10:48 -0400

    Switching world functionality

---
+++ Assets/Scripts/Made By Me/Switch_Worlds.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class Switch_Worlds : MonoBehaviour
+{
+
+    public float distance = 15f;
+    public GameObject reference;
+    private bool isFirstWorld = true;
+    private GameObject player;
+
+    private Collider2D collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.position.y > reference.transform.position.y) {
+            isFirstWorld = true;
+        }
+        else if (player.transform.position.y < reference.transform.position.y) {
+            isFirstWorld = false;
+        }
+        if (Input.GetKeyDown(KeyCode.E)) {
+            SwitchWorlds();
+        }
+    }
+
+    void SwitchWorlds() {
+        if (isFirstWorld) {
+            player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y - distance, player.transform.position.z);
+        } else {
+            player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + distance, player.transform.position.z);
+        }
+    }
+}
