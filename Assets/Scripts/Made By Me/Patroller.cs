36a5ab13362bbcc6cc6e5ecea2ce6401849ce137
Nicole Burg <32913338+nicolerburg@users.noreply.github.com>
2020-07-24 14:48:31 -0400

    Added patroller/moving platform script

---
+++ Assets/Scripts/Made By Me/Patroller.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Patroller : MonoBehaviour
+{
+    [Tooltip("Object will always move toward Point Afirst")]
+    public Transform pointA;
+    public Transform pointB;
+    public float speed = 1f;
+
+    private bool isMovingTowardsA = true;
+    private Connected_Objects connected;
+    private bool shouldMove;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        connected = gameObject.GetComponent<Connected_Objects>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, 0);
+        shouldMove = connected.shouldMove;
+        UnityEngine.Debug.Log(connected.shouldMove);
+        if (connected.shouldMove) {
+            Patrol();
+        }
+    }
+
+    void Patrol() {
+        if (isMovingTowardsA) {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, pointA.position, speed);
+        }
+        else {
+            this.transform.position = Vector3.MoveTowards(transform.position, pointB.position, speed);
+        }
+        if (this.transform.position == pointA.position) {
+            isMovingTowardsA = false;
+        }
+        else if (this.transform.position == pointB.position) {
+            isMovingTowardsA = true;
+        }
+    }
+}
