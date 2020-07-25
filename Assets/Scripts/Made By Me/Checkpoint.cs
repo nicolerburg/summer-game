69d67a2b16fdef2f0c02dc7293862efd0fcbc7bf
Nicole Burg <32913338+nicolerburg@users.noreply.github.com>
2020-07-25 13:06:48 -0400

    Added checkpoint script

---
+++ Assets/Scripts/Made By Me/Checkpoint.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+
+    private GameManager_Burg manager;
+    private Vector3 position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = FindObjectOfType<GameManager_Burg>();
+        position = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        manager.setRespawn(position);
+    }
+}
